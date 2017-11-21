07fbdf5ea097735813b4698092151a295f8577d7
grantspell <spellgrant@gmail.com>
2017-11-21 03:26:25 -0500

    ADDED | a small piece of text and the layout for `connect` page

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -4,12 +4,28 @@ import styled from 'styled-components';
 // COMPONENTS
 import NavBar from '../navBar/NavBar'
 
+// STYLES
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 100vw;
+    height: 50vh;
+    color: white;
+    font-family: 'Roboto Mono', monospace;
+    font-size: 10px;
+    letter-spacing: 5px;
+    margin-top: -15px;
+`
+
 class Connect extends Component {
     render() {
         return (
             <div>
                 <NavBar />
                 
+                <Wrapper>
+                    <div>HERE WILL BE ALL OF MY LINKS</div>
+                </Wrapper>
             </div>
         );
     }
