a0dbd8f3a5d4c6c51f39fc4dbe31dcbf70381b3b
grantspell <spellgrant@gmail.com>
2017-11-21 03:00:09 -0500

    ROUGH styling for `narrative` page

--- src/components/narrative/Narrative.js
+++ src/components/narrative/Narrative.js
@@ -8,11 +8,19 @@ import NavBar from '../navBar/NavBar'
 const NarrativeWrapper = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
     width: 100vw;
     height: 100vh;
 `
 const Content = styled.div`
-
+    margin-top: -50px;
+    width: 100vw;
+    height: 50vh;
+    background-image: url(https://i.imgur.com/100gwAO.png);
+    background-size: 700px;
+    background-repeat: no-repeat;
+    background-position: center;
 `
 
 class Narrative extends Component {
@@ -22,6 +30,9 @@ class Narrative extends Component {
                 <NavBar />
 
                 <Content>
+                    <p>
+                        Thank you for visiting!
+                    </p>
                 </Content>
                 
             </NarrativeWrapper>
