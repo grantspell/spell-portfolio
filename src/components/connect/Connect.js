cb0c4b14051a6ccaae8d6559becaa04c240fa294
grantspell <spellgrant@gmail.com>
2017-11-27 22:12:14 -0500

    CREATED | my v own LinkedIn badge for the `Connect` page

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: space-around;
     width: 100vw;
-    height: 50vh;
+    height: 40vh;
     color: white;
     font-family: 'Roboto Mono', monospace;
     font-size: 10px;
@@ -36,6 +36,35 @@ const EmailStyles = styled.div`
         text-decoration:  none;
     }
 `
+const LinkedIn = styled.div`
+    height: 200px;
+    background-color: yellow;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+
+    img {
+        height: 100px;
+        width: 100px;
+        border-radius: 100%;
+    }
+    .pLink {
+        background-color: white;
+        padding: 5px;
+        border: 5px double black;
+    }
+`
+const Text = styled.div`
+    color: black;
+    letter-spacing: 1px;
+    line-height: .5;
+    
+    p {
+        letter-spacing: 8px;
+    }
+`
 const Resume = styled.div`
 
 `
@@ -50,7 +79,16 @@ class Connect extends Component {
 
                     <EmailStyles>
                         <h3>LINKED IN</h3>
-                        <a href="https://www.linkedin.com/in/grantspell/">GRANT SPELL | PROFILE</a>
+                        <LinkedIn>
+                        <div>
+                        <img src="https://i.imgur.com/fq0kUPi.jpg" />
+                        </div>
+                        <Text>
+                            <h1>Grant Spell</h1>
+                            <p><a href="https://www.linkedin.com/in/grantspell/" target="_blank">/grantspell</a></p>
+                        </Text>
+                        <a className="pLink" href="https://www.linkedin.com/in/grantspell/">PROFILE</a>
+                        </LinkedIn>
                         <br/><br />
                         <h3>EMAIL ME</h3>
                         <a href="mailto: spellgrant@gmail.com">SPELLGRANT@GMAIL.COM</a>
